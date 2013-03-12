41cef1b9130fd9d2ec71a135e2eb9df4efe2fe58
feisky <feiskyer@gmail.com>
2013-03-12 13:23:23 +0800

    add DOWNLOAD_PREVIEW to control if download web previews

--- newsspider/newsspider/pipelines.py
+++ newsspider/newsspider/pipelines.py
@@ -11,28 +11,37 @@ import os
 import uuid
 
 class NewsspiderPipeline(object):
+
     def __init__(self):
         self.conn = MySQLdb.connect(user='root', 
                 passwd='feisky', db='news', host='localhost', 
                 charset="utf8", 
                 use_unicode=True)
         self.cursor = self.conn.cursor()
+        self.downloadPreview=False
+        self.pngPath='/var/scrapy'
+        self.filename=''
 
     def process_item(self, item, spider):
+        settings = spider.settings
+        if settings['DOWNLOAD_PREVIEW']:
+            self.downloadPreview=True
+            if settings['PNG_PATH']: 
+                self.pngPath=settings['PNG_PATH']
+
         url = item.get('url', '')
         if len(url)==0:
             return item
 
         try:
-            curdir = os.getcwdu()
-            filename = "%s/pngs/%s.png" % (curdir, str(uuid.uuid1()))
-
-            cmd = os.popen(u'''%s/webkit2png -x 1366 768 -F javascript "%s" -o "%s"''' % 
-                        (curdir, url, filename))
-            result = cmd.read()
-            if 'Failed' in result:
-                os.unlink(filename)
-            cmd.close()
+            if self.downloadPreview:
+                self.filename = "%s/%s.png" % (self.pngPath, str(uuid.uuid1()))
+                cmd = os.popen(u'''/usr/bin/webkit2png -x 1366 768 -F javascript "%s" -o "%s"''' % 
+                        (url, self.filename))
+                result = cmd.read()
+                if 'Failed' in result:
+                    os.unlink(filename)
+                cmd.close()
 
             if self.cursor.execute('''select * from news where url='%s' ''' % 
                     item.get('url', '')) == 0:
@@ -43,8 +52,8 @@ class NewsspiderPipeline(object):
                         url,
                         item.get('site', ''),
                         item.get('abstract', '').encode('utf-8'),
-                        item.get('created', time.strftime('%Y-%m-%d %H:%M:%S'),
-                        filename)))
+                        item.get('created', time.strftime('%Y-%m-%d %H:%M:%S')),
+                        self.filename.split('/')[-1]) )
                 self.conn.commit()
         except MySQLdb.Error, e:
             print "Error %d: %s" % (e.args[0], e.args[1])
--- newsspider/newsspider/settings.py
+++ newsspider/newsspider/settings.py
@@ -7,7 +7,19 @@
 #
 
 BOT_NAME = 'Newsspider'
-USER_AGENT = 'Newsspider+(+http://www.baidu.com/)'
+USER_AGENT = 'Newsspider+(+http://www.www.com/)'
+# Baiduspider+(+http://www.baidu.com/search/spider.htm")
+# Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+# Googlebot/2.1 (+http://www.googlebot.com/bot.html)
+# Googlebot/2.1 (+http://www.google.com/bot.html)
+# Mozilla/5.0 (compatible; Yahoo! Slurp China; http://misc.yahoo.com.cn/help.html")
+# Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp")
+# iaskspider/2.0(+http://iask.com/help/help_index.html")
+# Mozilla/5.0 (compatible; iaskspider/1.0; MSIE 6.0)
+# Sogou web spider/3.0(+http://www.sogou.com/docs/help/webmasters.htm#07")
+# Sogou Push Spider/3.0(+http://www.sogou.com/docs/help/webmasters.htm#07")
+# Mozilla/5.0 (compatible; YodaoBot/1.0; http://www.yodao.com/help/webmaster/spider/"; )
+# msnbot/1.0 (+http://search.msn.com/msnbot.htm")
 
 SPIDER_MODULES = ['newsspider.spiders']
 NEWSPIDER_MODULE = 'newsspider.spiders'
@@ -48,3 +60,6 @@ DNSCACHE_ENABLED = True
 
 #LOG_FILE = ''
 LOG_LEVEL = 'DEBUG'
+
+PNG_PATH = '/var/scrapy'
+DOWNLOAD_PREVIEW = False
